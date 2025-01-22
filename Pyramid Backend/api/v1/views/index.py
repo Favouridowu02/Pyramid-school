c13241ead3bdf9ababe48d590aa8b688693921a2
Favour Idowu <favouridowu02@gmail.com>
2025-01-22 12:06:47 +0100

    feat: Implement the index module to handle the basic routes
    - Implement /status endpoint to return the status of the api when it is active
    - implement /forbidden endpoint to handle forbidden access
    - Return 403 status code for forbidden requests

---
+++ Pyramid Backend/api/v1/views/index.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+    This module defines the basic routes for the Pyramid Backend API.
+    It includes endpoints for checking the status of the API and handling
+    unauthorized access.
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+    """ GET /api/v1/unauthorised
+    Return:
+        - Abort unauthorised
+    """
+    abort(401)
+
+
+@app_views.route("/forbidden", strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Return:
+      - Abort Forbidden
+    """
+    abort(403)
+
