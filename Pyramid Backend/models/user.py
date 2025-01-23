af53b52c56f490e5910f129249978d3bf37a4f4a
Favour Idowu <favouridowu02@gmail.com>
2025-01-23 15:46:38 +0100

    fix: Resolve the admin, student, and mentor model creation Issues

--- Pyramid Backend/models/user.py
+++ Pyramid Backend/models/user.py
@@ -6,29 +6,19 @@ from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 
-class User:
-    """
-        This class is used to create the Users Schema
-    """
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128),  nullable=True)
-    password = Column(String(128),  nullable=True)
-    email = Column(String(128), nullable=True)
 
-
-# This would be updated soon
-class Student(BaseModel, User, Base):
+class Student(BaseModel, Base):
     """
         This Class is used to create the Students Schema
     """
     __tablename__ = "students"
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128),  nullable=True)
+    password = Column(String(128),  nullable=True)
+    email = Column(String(128), nullable=True)
     xp = Column(Integer, nullable=True)
 
     student_projects = relationship("StudentProject", back_populates="student")
-    def __init__(self):
-        """ Initialize the Student Class
-        """
-        super().__init__()
 
 
 class Mentor(BaseModel, User, Base):
@@ -36,10 +26,10 @@ class Mentor(BaseModel, User, Base):
         This Class is used to create the Mentor Schema
     """
     __tablename__ = "mentors"
-    def __init__(self):
-        """ Initialize the Student Class
-        """
-        super().__init__()
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128),  nullable=True)
+    password = Column(String(128),  nullable=True)
+    email = Column(String(128), nullable=True)
 
 
 class Admin(BaseModel, User, Base):
@@ -47,7 +37,7 @@ class Admin(BaseModel, User, Base):
         This Class is used to create the Admin Schema
     """
     __tablename__ = "admins"
-    def __init__(self):
-        """ Initialize the Student Class
-        """
-        super().__init__()
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128),  nullable=True)
+    password = Column(String(128),  nullable=True)
+    email = Column(String(128), nullable=True)
