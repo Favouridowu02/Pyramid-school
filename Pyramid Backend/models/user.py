d7cc588ae4fe98d32bf2a593d5baf8c1c1c4aa42
Favour Idowu <favouridowu02@gmail.com>
2025-01-23 16:23:26 +0100

    fix: add username to Student Model
    - Implement the username in the Student Model

--- Pyramid Backend/models/user.py
+++ Pyramid Backend/models/user.py
@@ -14,6 +14,7 @@ class Student(BaseModel, Base):
     __tablename__ = "students"
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128),  nullable=True)
+    user_name = Column(String(128), nullable=True)
     password = Column(String(128),  nullable=True)
     email = Column(String(128), nullable=True)
     xp = Column(Integer, nullable=True)
