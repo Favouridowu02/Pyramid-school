9beb52124f7a6eee0ecfd6fc4a005dae7c276ccd
Favour Idowu <favouridowu02@gmail.com>
2025-01-24 23:36:20 +0100

    feat: Improve Course model schema
    - Add program_id as a foreign key to reference the Program model
    - Ensure proper relationships are defined for the Course model
    - Update docstrings for clarity and consistency

--- Pyramid Backend/models/course.py
+++ Pyramid Backend/models/course.py
@@ -2,10 +2,10 @@
 """
     This Model contains the Course class
 """
-from models.base_model import Base
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+
 
 class Course(BaseModel, Base):
     """
@@ -13,6 +13,7 @@ class Course(BaseModel, Base):
     """
     __tablename__ = "courses"
     name = Column(String(128), nullable=False)
-    projects = relationship("Project", back_populates="course")
     program_id = Column(String(60), ForeignKey('programs.id'), nullable=False)
+    
+    projects = relationship("Project", back_populates="course")
     program = relationship("Program", back_populates='courses')
