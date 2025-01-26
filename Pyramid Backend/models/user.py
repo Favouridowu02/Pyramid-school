d39972b71843239fc3f840b6c4414759b153e03f
Favour Idowu <favouridowu02@gmail.com>
2025-01-26 12:46:27 +0100

    feat: Define Student model schema
    - Add first_name, last_name, user_name, password, email, and xp columns
    - Ensure first_name, last_name, user_name, password, and email are required fields
    - Set user_name and email as unique fields
    - Set default value of xp to 0
    - Add docstrings for clarity and consistency

--- Pyramid Backend/models/user.py
+++ Pyramid Backend/models/user.py
@@ -3,7 +3,7 @@
     This Model contains the User class
 """
 from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -13,13 +13,13 @@ class Student(BaseModel, Base):
     """
     __tablename__ = "students"
     first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128),  nullable=True)
-    user_name = Column(String(128), nullable=True)
-    password = Column(String(128),  nullable=True)
-    email = Column(String(128), nullable=True)
-    xp = Column(Integer, nullable=True)
+    last_name = Column(String(128),  nullable=False)
+    user_name = Column(String(128), nullable=False, unique=True)
+    password = Column(String(128),  nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
+    xp = Column(Integer, nullable=False, default=0)
 
-    student_projects = relationship("StudentProject", back_populates="student")
+    # student_projects = relationship("StudentProject", back_populates="student")
 
 
 class Mentor(BaseModel, Base):
@@ -28,9 +28,9 @@ class Mentor(BaseModel, Base):
     """
     __tablename__ = "mentors"
     first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128),  nullable=True)
-    password = Column(String(128),  nullable=True)
-    email = Column(String(128), nullable=True)
+    last_name = Column(String(128),  nullable=False)
+    password = Column(String(128),  nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
 
 
 class Admin(BaseModel, Base):
@@ -39,6 +39,6 @@ class Admin(BaseModel, Base):
     """
     __tablename__ = "admins"
     first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128),  nullable=True)
-    password = Column(String(128),  nullable=True)
-    email = Column(String(128), nullable=True)
+    last_name = Column(String(128),  nullable=False)
+    password = Column(String(128),  nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
