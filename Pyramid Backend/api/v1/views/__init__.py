e8fcf78b00241b6b534768fb54ac575c457ae51f
Favour Idowu <favouridowu02@gmail.com>
2025-02-05 14:05:11 +0100

    feat: Add the importation of the api modules

--- Pyramid Backend/api/v1/views/__init__.py
+++ Pyramid Backend/api/v1/views/__init__.py
@@ -4,14 +4,17 @@
 """
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
-from api.v1.views.index import *
+# Import all the views
+from api.v1.views import index
 from api.v1.views.students import *
 from api.v1.views.mentors import *
+from api.v1.views.admins import *
+from api.v1.views.programs import *
 from api.v1.views.courses import *
-# from api.v1.views.programs import *
-# from api.v1.views.projects import *
-# from api.v1.views.tasks import *
-# from api.v1.views.session_auth import *
+from api.v1.views.projects import *
+from api.v1.views.tasks import *
+from api.v1.views.student_projects import *
+
+
