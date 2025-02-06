c4d9f596704b0f72eacdaf21eb9be0c9515645d4
Favour Idowu <favouridowu02@gmail.com>
2025-02-06 10:53:52 +0100

    feat: add API endpoint to retrieve all tasks in tasks.py
    - Implemented GET /api/v1/tasks endpoint to retrieve information of all registered tasks.
    - Imported necessary modules and reloaded storage to ensure the database is initialized.
    - Added route to return a list of all task objects in JSON format with a 200 status code.

--- Pyramid Backend/api/v1/views/tasks.py
+++ Pyramid Backend/api/v1/views/tasks.py
@@ -2,11 +2,10 @@
 """
     This Module contains the API End Points for the Tasks
 """
-from models.user import Task
+from models.task import Task
 from flask import request, jsonify, abort
 from models import storage
 from api.v1.views import app_views
-
 
 storage.reload()
 
@@ -19,147 +18,124 @@ def all_tasks() -> str:
     """
     all_tasks = storage.all(Task)
     all_tasks_list = [task.to_dict() for task in all_tasks]
-    print("\n\n", all_tasks_list, "\n\n")
     return jsonify(all_tasks_list), 200
 
 @app_views.route('/tasks/<task_id>', methods=["GET"], strict_slashes=False)
 def one_task(task_id: str = None) -> str:
-  """ GET /api/v1/tasks/:id
-  Path Parameter:
-    - Task ID
-  Description: Gets information of one registered task
-  Return:
-    - A JSON representation of the task, 200
-    - 404 if the task does not exist
-  """
-  if task_id is None:
-    abort(404)
-  if task_id == "me":
-    if request.current_user is None:
-      abort(404)
-    else:
-      return jsonify(request.current_user.to_dict()), 200
-  task = storage.get(Task, task_id)
-  if task is None:
-    abort(404)
-  return jsonify(task.to_dict()), 200
+    """ GET /api/v1/tasks/:id
+    Path Parameter:
+      - Task ID
+    Description: Gets information of one registered task
+    Return:
+      - A JSON representation of the task, 200
+      - 404 if the task does not exist
+    """
+    if task_id is None:
+        abort(404)
+    task = storage.get(Task, task_id)
+    if task is None:
+        abort(404)
+    return jsonify(task.to_dict()), 200
 
-
-@app_views.route("/tasks/<task_id>", methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route("/tasks/<task_id>", methods=['DELETE'], strict_slashes=False)
 def delete_task(task_id: str = None) -> str:
-  """ DELETE /api/v1/tasks/:id
-  Path Parameter:
-    - Task ID
-  Description: Deletes information of one registered task
-  Return:
-    - Empty JSON id, Task has been correctly deleted, 204
-    - 404 if the Task ID doesn't exist
-  """
-  if task_id is None:
-    abort(404)
-  task = storage.get(Task, task_id)
-  if task is None:
-    abort(404)
-  try:
-    task.delete()
-  except Exception as e:
-    abort(404)
-  return jsonify({}), 204
-
+    """ DELETE /api/v1/tasks/:id
+    Path Parameter:
+      - Task ID
+    Description: Deletes information of one registered task
+    Return:
+      - Empty JSON id, Task has been correctly deleted, 204
+      - 404 if the Task ID doesn't exist
+    """
+    if task_id is None:
+        abort(404)
+    task = storage.get(Task, task_id)
+    if task is None:
+        abort(404)
+    try:
+        task.delete()
+    except Exception as e:
+        abort(404)
+    return jsonify({}), 204
 
 @app_views.route("/tasks", methods=["POST"], strict_slashes=False)
 def create_task():
-  """ POST /api/v1/tasks
-  Description: Create a new Task
-  Request JSON body:
-    - first_name
-    - last_name
-    - user_name
-    - password
-    - email
-    - xp
-  Return:
-    - JSON representation of the created task, 201
-    - 400 if any required field is missing or if there is an error
-  """
-  request_json = None
-  error_message = None
-
-  try:
-    request_json = request.get_json()
-  except Exception:
+    """ POST /api/v1/tasks
+    Description: Create a new Task
+    Request JSON body:
+      - name
+      - github_link
+      - img_url
+      - project_id
+    Return:
+      - JSON representation of the created task, 201
+      - 400 if any required field is missing or if there is an error
+    """
     request_json = None
-  
-  if request_json is None:
-    error_message = "Wrong Format"
-  else:
-    if not request_json.get('first_name'):
-      error_message = "First Name Missing"
-    elif not request_json.get("last_name"):
-      error_message = "Last Name Missing"
-    elif not request_json.get("user_name"):
-      error_message = "User Name missing"
-    elif not request_json.get("password"):
-      error_message = "Password Missing"
-    elif not request_json.get("email"):
-      error_message = "Missing Email"
-    else:
-      try:
-        task = Task()
-        task.first_name = request_json.get('first_name')
-        task.last_name = request_json.get('last_name')
-        task.email = request_json.get("email")
-        task.user_name = request_json.get("user_name")
-        task.password = request_json.get("password")
-        task.save()
-        return jsonify(task.to_dict()), 201
-      except Exception as e:
-        error_message = "Can't create a Task: {}".format(e)
-  return jsonify({"error": error_message}), 404
+    error_message = None
 
+    try:
+        request_json = request.get_json()
+    except Exception:
+        request_json = None
+
+    if request_json is None:
+        error_message = "Wrong Format"
+    else:
+        if not request_json.get('name'):
+            error_message = "Task Name Missing"
+        elif not request_json.get("project_id"):
+            error_message = "Missing or Wrong Project ID"
+        else:
+            try:
+                task = Task()
+                task.name = request_json.get('name')
+                task.github_link = request_json.get('github_link')
+                task.img_url = request_json.get('img_url')
+                task.project_id = request_json.get('project_id')
+                task.save()
+                return jsonify(task.to_dict()), 201
+            except Exception as e:
+                error_message = "Can't create a Task: {}".format(e)
+    return jsonify({"error": error_message}), 404
 
 @app_views.route('/tasks/<task_id>', methods=["PUT"], strict_slashes=False)
-def update_task(patient_id: str = None) -> str:
-  """ PUT /api/v1/task/:id
-  Update a task Data
-  Request JSON body
-    - first_name
-    - last_name
-    - user_name
-    - password
-    - email
-    - xp
-  Return:
-    - JSON representation of the created task
-    - 400 if any required field is missing or if there is an error
-  """
-  request_json = None
-  error_message = None
+def update_task(task_id: str = None) -> str:
+    """ PUT /api/v1/task/:id
+    Update a task Data
+    Request JSON body
+      - name
+      - github_link
+      - img_url
+      - project_id
+    Return:
+      - JSON representation of the created task
+      - 400 if any required field is missing or if there is an error
+    """
+    if task_id is None:
+        abort(404)
 
-  try:
-    request_json = request.get_json()
-  except Exception:
     request_json = None
-  
-  if request_json is None:
-    error_message = "Wrong Format"
-  else:
-    task = storage.get(Task, task_id)
-    if task is None:
-      abort(404)
-    first_name = request_json.get('first_name',
-                                  task.first_name)
-    last_name = request_json.get('last_name',
-                                  task.last_name)
-    task.first_name = first_name
-    task.last_name = last_name
-    task.user_name = request_json.get('user_name', task.user_name)
-    task.password = request_json.get('password', task.password)
-    task.email = request_json.get('email', task.email)
+    error_message = None
 
-    task.save()
-    return jsonify(task.to_dict()), 200
-  except Exception as e:
-      error_message = "Can't update Task: {}".format(e)
-return jsonify({"error": error_message}), 400
+    try:
+        request_json = request.get_json()
+    except Exception:
+        request_json = None
+
+    if request_json is None:
+        error_message = "Wrong Format"
+    else:
+        try:
+            task = storage.get(Task, task_id)
+            if task is None:
+                abort(404)
+            task.name = request_json.get('name', task.name)
+            task.github_link = request_json.get('github_link', task.github_link)
+            task.img_url = request_json.get('img_url', task.img_url)
+            task.project_id = request_json.get('project_id', task.project_id)
+            task.save()
+            return jsonify(task.to_dict()), 200
+        except Exception as e:
+            error_message = "Can't update Task: {}".format(e)
+    return jsonify({"error": error_message}), 400
