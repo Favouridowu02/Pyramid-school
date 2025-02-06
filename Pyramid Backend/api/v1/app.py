a85bd97c65905b5b5bdf80ca02dc45a0d0bab4c8
Favour Idowu <favouridowu02@gmail.com>
2025-02-06 11:00:29 +0100

    feat: set up Flask application and configure API in app.py
    - Configured Flask application for the Pyramid Backend API.
    - Initialized the database and registered necessary routes and blueprints.
    - Added CORS support for API endpoints.
    - Implemented conditional import for BasicAuth based on AUTH_TYPE environment variable.

--- Pyramid Backend/api/v1/app.py
+++ Pyramid Backend/api/v1/app.py
@@ -9,6 +9,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from os import getenv
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -57,12 +58,10 @@ def before_request() -> str:
         abort(403)
     request.current_user = auth.current_user(request)
 
-@app.errorhandler(404)
-def not_found(errorhandler) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the storage on teardown"""
+    storage.close()
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
@@ -76,4 +75,4 @@ if __name__ == "__main__":
     """
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port, debug=True, threaded=True)
