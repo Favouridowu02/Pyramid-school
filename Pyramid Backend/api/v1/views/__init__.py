8121569e7ee8f841fea94338f4fb9ec2a03ffbfb
Favour Idowu <favouridowu02@gmail.com>
2025-02-06 11:04:55 +0100

    fix: resolve the importation of the index module
    - Updated the module to properly import the endpoints form the index module

--- Pyramid Backend/api/v1/views/__init__.py
+++ Pyramid Backend/api/v1/views/__init__.py
@@ -7,7 +7,7 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import all the views
-from api.v1.views import index
+from api.v1.views.index import *
 from api.v1.views.students import *
 from api.v1.views.mentors import *
 from api.v1.views.admins import *
